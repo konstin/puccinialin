e396192446d4ff393c3912fa2f069a23681cd24b
Winston H. <winstxnhdw@gmail.com>
2026-04-10 22:53:22 +0800

    Fix Windows ARM64 detection (#33)

--- src/puccinialin/_target.py
+++ src/puccinialin/_target.py
@@ -99,6 +99,9 @@ def get_triple(file: typing.IO) -> str:
     # Windows x86
     elif sysconfig_platform == "win32":
         target = "i686-pc-windows-msvc"
+    # Windows ARM64
+    elif sysconfig_platform in ["win_arm64", "win-arm64"]:
+        target = "aarch64-pc-windows-msvc"
     else:
         print(f"Unsupported platform: {sysconfig_platform}", file=file)
         sys.exit(1)
